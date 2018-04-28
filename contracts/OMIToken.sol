366592a1f110e5a614b59f7e8550a1e9da0d8437
Mikel Duffy <mikelduffy1@gmail.com>
2018-04-28 12:59:23 +0800

    Implements OMI token contract

---
+++ contracts/OMIToken.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.18;
+
+import "../node_modules/zeppelin-solidity/contracts/token/ERC20/CappedToken.sol";
+
+contract OMIToken is CappedToken {
+  string public constant name = "Ecomi Token";
+  string public constant symbol = "OMI";
+  uint256 public decimals = 18;
+
+  function OMIToken() public CappedToken(1000000000) {}
+}
