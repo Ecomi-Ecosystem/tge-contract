7fcc4cfe7f4b08f93172eaab60d3387be25b4242
Mikel Duffy <mikelduffy1@gmail.com>
2018-04-28 19:59:36 +0800

    Makes token contract pausable

--- contracts/OMIToken.sol
+++ contracts/OMIToken.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.4.18;
 
 import "../node_modules/zeppelin-solidity/contracts/token/ERC20/CappedToken.sol";
+import "../node_modules/zeppelin-solidity/contracts/token/ERC20/PausableToken.sol";
 
-contract OMIToken is CappedToken {
+contract OMIToken is CappedToken, PausableToken {
   string public constant name = "Ecomi Token";
   string public constant symbol = "OMI";
   uint256 public decimals = 18;
