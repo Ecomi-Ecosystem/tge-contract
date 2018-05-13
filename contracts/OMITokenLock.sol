1059a1fbd3de22c2a454d8b3cfe475ee5085f6ed
Mikel Duffy <mikelduffy1@gmail.com>
2018-05-14 07:55:10 +0800

    Updates token lock and tests

--- contracts/OMITokenLock.sol
+++ contracts/OMITokenLock.sol
@@ -5,78 +5,167 @@ import "../node_modules/zeppelin-solidity/contracts/ownership/Ownable.sol";
 import "../node_modules/zeppelin-solidity/contracts/math/SafeMath.sol";
 import "../node_modules/zeppelin-solidity/contracts/lifecycle/Pausable.sol";
 
-/**
-@title OMITokenLock
-@dev OMITokenLock is a token holder contract that will allow multiple beneficiaries to extract the tokens after a given release time. It is a modification of the OpenZeppenlin TokenLock to allow for one token lock smart contract for many beneficiaries. 
- */
+/// @title OMITokenLock
+/// @author Mikel Duffy - <mikel@ecomi.com>
+/// @dev OMITokenLock is a token holder contract that will allow multiple beneficiaries to extract the tokens after a given release time. It is a modification of the OpenZeppenlin TokenLock to allow for one token lock smart contract for many beneficiaries.
 contract OMITokenLock is Ownable, Pausable {
   using SafeMath for uint256;
 
-  function OMITokenLock (OMIToken _token) public {
-    token = _token;
-  }
-
-  OMIToken public token;
-
-  event CrowdsaleFinished();
-
-  bool public crowdsaleFinished = false;
-
-  uint256 public crowdsaleEndTime;
-
   /*
-  @notice Marks the crowdsale as being finished and sets the crowdsale finish date.
-  */
-  function finishCrowdsale() public onlyOwner whenNotPaused {
-    require(!crowdsaleFinished);
-    crowdsaleFinished = true;
-    crowdsaleEndTime = now;
-    CrowdsaleFinished();
-  }
+   *  Storage
+   */
+  OMIToken public token;
+  address public allowanceProvider;
+  address public crowdsale;
+  bool public crowdsaleFinished = false;
+  uint256 public crowdsaleEndTime;
 
   struct Lock {
     uint256 amount;
     uint256 lockDuration;
     bool released;
+    bool revoked;
   }
-
   struct TokenLockVault {
     address beneficiary;
     uint256 tokenBalance;
     uint256 lockIndex;
     Lock[] locks;
   }
-
   mapping(address => TokenLockVault) public tokenLocks;
-
   address[] public lockIndexes;
-
-  uint256 public contractTokenBalance = 0;
+  uint256 public totalTokensLocked;
 
   /*
-  @notice Gets the total amount of tokens for a given address.
-  @param _beneficiary The address for which to look up the total token amount.
-  */
-  function getTokenBalance(address _beneficiary) public view returns (uint) {
+   *  Modifiers
+   */
+  modifier ownerOrCrowdsale () {
+    require(msg.sender == owner || msg.sender == crowdsale);
+    _;
+  }
+
+  /*
+   *  Events
+   */
+  event LockedTokens(address indexed beneficiary, uint256 amount, uint256 releaseTime);
+  event UnlockedTokens(address indexed beneficiary, uint256 amount);
+  event FinishedCrowdsale();
+
+  /*
+   *  Public Functions
+   */
+  /// @dev Constructor function
+  function OMITokenLock (OMIToken _token) public {
+    token = _token;
+  }
+
+  /// @dev Sets the crowdsale address to allow authorize locking permissions
+  /// @param _crowdsale The address of the crowdsale
+  function setCrowdsaleAddress (address _crowdsale)
+    public
+    onlyOwner
+    returns (bool)
+  {
+    crowdsale = _crowdsale;
+    return true;
+  }
+
+  /// @dev Sets the token allowance provider address
+  /// @param _allowanceProvider The address of the token allowance provider
+  function setAllowanceAddress (address _allowanceProvider)
+    public
+    onlyOwner
+    returns (bool)
+  {
+    allowanceProvider = _allowanceProvider;
+    return true;
+  }
+
+  /// @dev Marks the crowdsale as being finished and sets the crowdsale finish date
+  function finishCrowdsale()
+    public
+    ownerOrCrowdsale
+    whenNotPaused
+  {
+    require(!crowdsaleFinished);
+    crowdsaleFinished = true;
+    crowdsaleEndTime = now;
+    FinishedCrowdsale();
+  }
+
+  /// @dev Gets the total amount of tokens for a given address
+  /// @param _beneficiary The address for which to look up the total token amount
+  function getTokenBalance(address _beneficiary)
+    public
+    view
+    returns (uint)
+  {
     return tokenLocks[_beneficiary].tokenBalance;
   }
 
-  event LockedTokens(address indexed beneficiary, uint256 amount, uint256 releaseTime);
+  /// @dev Gets the total number of locks for a given address
+  /// @param _beneficiary The address for which to look up the total number of locks
+  function getNumberOfLocks(address _beneficiary)
+    public
+    view
+    returns (uint)
+  {
+    return tokenLocks[_beneficiary].locks.length;
+  }
 
-  /*
-  @notice Locks tokens for a given beneficiary.
-  @param _beneficiary The address to which the tokens will be released.
-  @param _lockDuration The duration of time that must elapse after the crowdsale end date.
-  @param _tokens The amount of tokens to be locked. 
-  */
-  function lockTokens(address _beneficiary, uint256 _lockDuration, uint256 _tokens) external onlyOwner whenNotPaused  {
+  /// @dev Gets the lock at a given index for a given address
+  /// @param _beneficiary The address used to look up the lock
+  /// @param _lockIndex The index used to look up the lock
+  function getLockByIndex(address _beneficiary, uint256 _lockIndex)
+    public
+    view
+    returns (uint256 amount, uint256 lockDuration, bool released, bool revoked)
+  {
+    require(_lockIndex >= 0);
+    require(_lockIndex <= tokenLocks[_beneficiary].locks.length.sub(1));
+
+    return (
+      tokenLocks[_beneficiary].locks[_lockIndex].amount,
+      tokenLocks[_beneficiary].locks[_lockIndex].lockDuration,
+      tokenLocks[_beneficiary].locks[_lockIndex].released,
+      tokenLocks[_beneficiary].locks[_lockIndex].revoked
+    );
+  }
+
+  /// @dev Revokes the lock at a given index for a given address
+  /// @param _beneficiary The address used to look up the lock
+  /// @param _lockIndex The lock index to be revoked
+  function revokeLockByIndex(address _beneficiary, uint256 _lockIndex)
+    public
+    onlyOwner
+    returns (bool)
+  {
+    require(_lockIndex >= 0);
+    require(_lockIndex <= tokenLocks[_beneficiary].locks.length.sub(1));
+    require(!tokenLocks[_beneficiary].locks[_lockIndex].revoked);
+
+    tokenLocks[_beneficiary].locks[_lockIndex].revoked = true;
+
+    return true;
+  }
+
+  /// @dev Locks tokens for a given beneficiary
+  /// @param _beneficiary The address to which the tokens will be released
+  /// @param _lockDuration The duration of time that must elapse after the crowdsale end date
+  /// @param _tokens The amount of tokens to be locked
+  function lockTokens(address _beneficiary, uint256 _lockDuration, uint256 _tokens)
+    external
+    ownerOrCrowdsale
+    whenNotPaused
+  {
     // Lock duration must be greater than zero seconds
     require(_lockDuration >= 0);
     // Token amount must be greater than zero
     require(_tokens > 0);
-    // Tokens must be transfered to this contract prior to locking them
-    uint256 newTokenBalance = token.balanceOf(address(this));
-    require(_tokens == newTokenBalance.sub(contractTokenBalance));
+
+    // Token Lock must have a sufficient allowance prior to creating locks
+    uint256 tokenAllowance = token.allowance(allowanceProvider, address(this));
+    require(_tokens.add(totalTokensLocked) <= tokenAllowance);
 
     TokenLockVault storage lock = tokenLocks[_beneficiary];
 
@@ -88,84 +177,43 @@ contract OMITokenLock is Ownable, Pausable {
     }
 
     // Add the lock
-    lock.locks.push(Lock(_tokens, _lockDuration, false));
+    lock.locks.push(Lock(_tokens, _lockDuration, false, false));
 
     // Update the total tokens for this beneficiary
     lock.tokenBalance = lock.tokenBalance.add(_tokens);
 
     // Update the number of locked tokens
-    contractTokenBalance = newTokenBalance; 
+    totalTokensLocked = _tokens.add(totalTokensLocked);
 
     LockedTokens(_beneficiary, _tokens, _lockDuration);
   }
 
-  event UnlockedTokens(address indexed beneficiary, uint256 amount);
-
-  /* 
-  @notice Reviews and releases token for a given beneficiary. 
-  */
-  function _release(address _beneficiary) internal whenNotPaused returns (bool) {
-    TokenLockVault memory lock = tokenLocks[_beneficiary];
-    require(lock.beneficiary == _beneficiary);
-
-    bool hasUnDueLocks = false;
-    bool hasReleasedToken = false;
-
-    for (uint256 i = 0; i < lock.locks.length; i = i.add(1)) {
-      Lock memory currentLock = lock.locks[i];
-      // Skip any locks which are already released 
-      if (currentLock.released) {
-        continue;
-      }
-      
-      // Skip any locks that are not due for release
-      if (crowdsaleEndTime.add(currentLock.lockDuration) >= now) {
-        hasUnDueLocks = true;
-        continue;
-      }
-
-      // The amount of tokens to transfer must be less than the number of locked tokens
-      require(currentLock.amount <= token.balanceOf(address(this)));
-
-      // Release Tokens
-      UnlockedTokens(msg.sender, currentLock.amount);
-      hasReleasedToken = true;
-      tokenLocks[_beneficiary].locks[i].released = true;
-      tokenLocks[_beneficiary].tokenBalance = tokenLocks[_beneficiary].tokenBalance.sub(currentLock.amount);
-      contractTokenBalance = contractTokenBalance.sub(currentLock.amount);
-      assert(token.transfer(msg.sender, currentLock.amount));
-    }
-
-    // If there are no future locks to be released, delete the lock vault
-    if (!hasUnDueLocks) {
-      delete tokenLocks[msg.sender];
-      lockIndexes[lock.lockIndex] = 0x0;
-    }
-
-    return hasReleasedToken;
-  }
-
-  /*
-  @notice Transfers any tokens held in a timelock vault to beneficiary if they are due for release.
-  */
-  function releaseTokens() public whenNotPaused returns(bool) {
+  /// @dev Transfers any tokens held in a timelock vault to beneficiary if they are due for release.
+  function releaseTokens()
+    public
+    whenNotPaused
+    returns(bool)
+  {
     require(crowdsaleFinished);
     require(_release(msg.sender));
     return true;
   }
 
-  /*
-  @notice Transfers tokens held by timelock to all beneficiaries within the provided range.
-  @param from the start lock index
-  @param to the end lock index
-  */
-  function releaseAll(uint256 from, uint256 to) external whenNotPaused onlyOwner returns (bool) {
-    require(from >= 0);
-    require(from < to);
-    require(to <= lockIndexes.length);
+  /// @dev Transfers tokens held by timelock to all beneficiaries within the provided range.
+  /// @param _from the start lock index
+  /// @param _to the end lock index
+  function releaseAll(uint256 _from, uint256 _to)
+    external
+    whenNotPaused
+    onlyOwner
+    returns (bool)
+  {
+    require(_from >= 0);
+    require(_from < _to);
+    require(_to <= lockIndexes.length);
     require(crowdsaleFinished);
 
-    for (uint256 i = from; i < to; i = i.add(1)) {
+    for (uint256 i = _from; i < _to; i = i.add(1)) {
       address _beneficiary = lockIndexes[i];
 
       //Skip any previously removed locks
@@ -176,5 +224,55 @@ contract OMITokenLock is Ownable, Pausable {
       require(_release(_beneficiary));
     }
     return true;
+  }
+
+  /*
+   *  Internal Functions
+   */
+  /// @dev Reviews and releases token for a given beneficiary
+  /// @param _beneficiary address for which a token release should be attempted
+  function _release(address _beneficiary)
+    internal
+    whenNotPaused
+    returns (bool)
+  {
+    TokenLockVault memory lock = tokenLocks[_beneficiary];
+    require(lock.beneficiary == _beneficiary);
+
+    bool hasUnDueLocks = false;
+    bool hasReleasedToken = false;
+
+    for (uint256 i = 0; i < lock.locks.length; i = i.add(1)) {
+      Lock memory currentLock = lock.locks[i];
+      // Skip any locks which are already released or revoked
+      if (currentLock.released || currentLock.revoked) {
+        continue;
+      }
+
+      // Skip any locks that are not due for release
+      if (crowdsaleEndTime.add(currentLock.lockDuration) >= now) {
+        hasUnDueLocks = true;
+        continue;
+      }
+
+      // The amount of tokens to transfer must be less than the number of locked tokens
+      require(currentLock.amount <= token.allowance(allowanceProvider, address(this)));
+
+      // Release Tokens
+      UnlockedTokens(msg.sender, currentLock.amount);
+      hasReleasedToken = true;
+      tokenLocks[_beneficiary].locks[i].released = true;
+      tokenLocks[_beneficiary].tokenBalance = tokenLocks[_beneficiary].tokenBalance.sub(currentLock.amount);
+      totalTokensLocked = totalTokensLocked.sub(currentLock.amount);
+      assert(token.transferFrom(allowanceProvider, msg.sender, currentLock.amount));
+    }
+
+    // If there are no future locks to be released, delete the lock vault
+    if (!hasUnDueLocks) {
+      delete tokenLocks[msg.sender];
+      lockIndexes[lock.lockIndex] = 0x0;
+    }
+
+    return hasReleasedToken;
   }
 }
--- test/OMITokenLock.test.js
+++ test/OMITokenLock.test.js
@@ -22,6 +22,7 @@ contract('OMITokenLock', accounts => {
   beforeEach(async () => {
     token = await OMIToken.new({ from: owner })
     tokenLock = await OMITokenLock.new(token.address, { from: owner })
+    await tokenLock.setAllowanceAddress(owner)
     totalGas = 0
   })
 
@@ -30,15 +31,17 @@ contract('OMITokenLock', accounts => {
     balance.should.be.bignumber.equal(amount)
   }
 
-  const contractBalanceShouldBe = async amount => {
-    const balance = await tokenLock.contractTokenBalance().should.be.fulfilled
+  const totalTokensLockedShouldBe = async amount => {
+    const balance = await tokenLock.totalTokensLocked().should.be.fulfilled
     balance.should.be.bignumber.equal(amount)
   }
 
-  const mintToTokenLock = async amount => {
-    const result = await token.mint(tokenLock.address, amount).should.be
-      .fulfilled
-    totalGas += result.receipt.gasUsed
+  const mintAndAllow = async amount => {
+    const result1 = await token.mint(owner, amount).should.be.fulfilled
+    const result2 = await token.increaseApproval(tokenLock.address, amount, {
+      from: owner,
+    })
+    totalGas += result1.receipt.gasUsed + result2.receipt.gasUsed
   }
 
   const lockTokens = async (to, duration, amount) => {
@@ -48,8 +51,8 @@ contract('OMITokenLock', accounts => {
     totalGas += result.receipt.gasUsed
   }
 
-  const mintToTokenLockAndLockTokens = async (to, duration, amount) => {
-    await mintToTokenLock(amount).should.be.fulfilled
+  const mintAllowAndLockTokens = async (to, duration, amount) => {
+    await mintAndAllow(amount).should.be.fulfilled
     await lockTokens(to, duration, amount).should.be.fulfilled
   }
 
@@ -80,23 +83,29 @@ contract('OMITokenLock', accounts => {
     await increaseTimeTo(crowdsaleEndTime + duration).should.be.fulfilled
   }
 
+  it('should store an allowance provider', async () => {
+    const allowanceProvider = await tokenLock.allowanceProvider().should.be
+      .fulfilled
+    allowanceProvider.should.equal(owner)
+  })
+
   it('should keep track of the number of tokens locked', async () => {
-    await contractBalanceShouldBe(0)
-    await mintToTokenLockAndLockTokens(beneficiary1, duration.hours(1), 3)
-    await contractBalanceShouldBe(3)
-    await mintToTokenLockAndLockTokens(beneficiary1, duration.hours(1), 6)
-    await contractBalanceShouldBe(9)
+    await totalTokensLockedShouldBe(0)
+    await mintAllowAndLockTokens(beneficiary1, duration.hours(1), 3)
+    await totalTokensLockedShouldBe(3)
+    await mintAllowAndLockTokens(beneficiary1, duration.hours(1), 6)
+    await totalTokensLockedShouldBe(9)
     await finishCrowdsaleAndIncreaseTimeTo(duration.hours(3))
     await releaseTokens(beneficiary1)
-    await contractBalanceShouldBe(0)
+    await totalTokensLockedShouldBe(0)
   })
 
   it('should be pausable', async () => {
-    await mintToTokenLockAndLockTokens(beneficiary1, duration.hours(1), 100)
+    await mintAllowAndLockTokens(beneficiary1, duration.hours(1), 100)
 
     await tokenLock.pause({ from: owner }).should.be.fulfilled
 
-    await mintToTokenLock(100)
+    await mintAndAllow(100)
     await tokenLock.lockTokens(beneficiary1, duration.hours(1), 100, {
       from: owner,
     }).should.be.rejected
@@ -126,8 +135,8 @@ contract('OMITokenLock', accounts => {
 
     let totalTokens = await tokenLock.getTokenBalance(beneficiary1)
     totalTokens.should.be.bignumber.equal(0)
-    await mintToTokenLockAndLockTokens(beneficiary1, duration.hours(1), 3)
-    await mintToTokenLockAndLockTokens(beneficiary1, duration.hours(3), 3)
+    await mintAllowAndLockTokens(beneficiary1, duration.hours(1), 3)
+    await mintAllowAndLockTokens(beneficiary1, duration.hours(3), 3)
     totalTokens = await tokenLock.getTokenBalance(beneficiary1)
     totalTokens.should.be.bignumber.equal(6)
 
@@ -136,22 +145,22 @@ contract('OMITokenLock', accounts => {
     totalTokens.should.be.bignumber.equal(3)
   })
 
-  it('should only allow locking tokens that have been transfered to the token lock contract', async () => {
+  it('should only allow locking tokens that have been provided as an allowance to the token lock contract', async () => {
     await tokenLock.lockTokens(beneficiary1, duration.hours(1), 1, {
       from: owner,
     }).should.be.rejected
 
-    await mintToTokenLockAndLockTokens(beneficiary1, duration.hours(1), 1)
+    await mintAllowAndLockTokens(beneficiary1, duration.hours(1), 1)
 
     await tokenLock.lockTokens(beneficiary1, duration.hours(1), 1, {
       from: owner,
     }).should.be.rejected
 
-    await mintToTokenLockAndLockTokens(beneficiary1, duration.hours(1), 1)
+    await mintAllowAndLockTokens(beneficiary1, duration.hours(1), 1)
   })
 
   it('should only allow owner to lock tokens', async () => {
-    await mintToTokenLock(1)
+    await mintAndAllow(1)
 
     await tokenLock.lockTokens(beneficiary1, duration.hours(1), 1, {
       from: notOwner,
@@ -160,9 +169,35 @@ contract('OMITokenLock', accounts => {
     await lockTokens(beneficiary1, duration.hours(1), 1)
   })
 
+  it('should only allow owner to revoke locked tokens', async () => {
+    await mintAndAllow(1)
+
+    await tokenLock.lockTokens(beneficiary1, duration.hours(1), 1, {
+      from: owner,
+    }).should.be.fulfilled
+
+    await tokenLock.revokeLockByIndex(beneficiary1, 0, { from: notOwner })
+      .should.be.rejected
+  })
+
+  it('should allow owner to revoke locked tokens', async () => {
+    await mintAndAllow(1)
+
+    await tokenLock.lockTokens(beneficiary1, duration.hours(1), 1, {
+      from: owner,
+    }).should.be.fulfilled
+
+    await tokenLock.revokeLockByIndex(beneficiary1, 0, { from: owner }).should
+      .be.fulfilled
+
+    const lock = await tokenLock.getLockByIndex(beneficiary1, 0).should.be
+      .fulfilled
+    lock[3].should.be.true
+  })
+
   it('should allow owner to lock tokens after the crowdsale is finished', async () => {
     await finishCrowdsale()
-    await mintToTokenLockAndLockTokens(beneficiary1, duration.hours(1), 1)
+    await mintAllowAndLockTokens(beneficiary1, duration.hours(1), 1)
   })
 
   it('should only allow the owner to finish the crowdsale', async () => {
@@ -175,7 +210,7 @@ contract('OMITokenLock', accounts => {
   })
 
   it('should only allow tokens to be released after the crowdsale is finished', async () => {
-    await mintToTokenLockAndLockTokens(beneficiary1, duration.hours(1), 1)
+    await mintAllowAndLockTokens(beneficiary1, duration.hours(1), 1)
 
     await increaseTimeTo(latestTime() + duration.hours(2)).should.be.fulfilled
 
@@ -193,8 +228,8 @@ contract('OMITokenLock', accounts => {
 
   it('should wait until lock duration is completed before releasing tokens', async () => {
     const crowdsaleEndTime = await finishCrowdsale()
-    await mintToTokenLockAndLockTokens(beneficiary1, duration.hours(3), 3)
-    await mintToTokenLockAndLockTokens(beneficiary1, duration.hours(6), 6)
+    await mintAllowAndLockTokens(beneficiary1, duration.hours(3), 3)
+    await mintAllowAndLockTokens(beneficiary1, duration.hours(6), 6)
 
     await tokenLock.releaseTokens({ from: beneficiary1 }).should.be.rejected
 
@@ -213,7 +248,7 @@ contract('OMITokenLock', accounts => {
   })
 
   it('should reject if a beneficiary has no locks', async () => {
-    await mintToTokenLock(1)
+    await mintAndAllow(1)
     await finishCrowdsaleAndIncreaseTimeTo(duration.hours(2))
 
     await tokenLock.releaseTokens({ from: beneficiary1 }).should.be.rejected
@@ -222,19 +257,19 @@ contract('OMITokenLock', accounts => {
     await tokenLock.releaseTokens({ from: beneficiary1 }).should.be.fulfilled
     await tokenLock.releaseTokens({ from: beneficiary1 }).should.be.rejected
 
-    await mintToTokenLockAndLockTokens(beneficiary1, duration.hours(1), 1)
+    await mintAllowAndLockTokens(beneficiary1, duration.hours(1), 1)
     await tokenLock.releaseTokens({ from: beneficiary1 }).should.be.fulfilled
     await tokenLock.releaseTokens({ from: beneficiary1 }).should.be.rejected
   })
 
   it('should only allow the owner to call releaseAll', async () => {
-    await mintToTokenLockAndLockTokens(beneficiary1, duration.hours(1), 1)
+    await mintAllowAndLockTokens(beneficiary1, duration.hours(1), 1)
     await finishCrowdsaleAndIncreaseTimeTo(duration.hours(2))
     await tokenLock.releaseAll(0, 0, { from: notOwner }).should.be.rejected
   })
 
   it('should only allow valid index ranges for releaseAll', async () => {
-    await mintToTokenLockAndLockTokens(beneficiary1, duration.hours(1), 1)
+    await mintAllowAndLockTokens(beneficiary1, duration.hours(1), 1)
     await finishCrowdsaleAndIncreaseTimeTo(duration.hours(2))
     await tokenLock.releaseAll(-1, 0, { from: owner }).should.be.rejected
     await tokenLock.releaseAll(1, 0, { from: owner }).should.be.rejected
@@ -248,24 +283,12 @@ contract('OMITokenLock', accounts => {
     const chunkSize = totalAccounts / 2
 
     for (let i = 0; i < totalAccounts; i++) {
-      await mintToTokenLockAndLockTokens(
-        accounts[i],
-        duration.weeks(1),
-        1600000
-      )
-      await mintToTokenLockAndLockTokens(
-        accounts[i],
-        duration.weeks(4),
-        1600000
-      )
-      await mintToTokenLockAndLockTokens(
-        accounts[i],
-        duration.weeks(8),
-        1600000
-      )
+      await mintAllowAndLockTokens(accounts[i], duration.weeks(1), 1600000)
+      await mintAllowAndLockTokens(accounts[i], duration.weeks(4), 1600000)
+      await mintAllowAndLockTokens(accounts[i], duration.weeks(8), 1600000)
     }
 
-    await contractBalanceShouldBe(totalAccounts * 1600000 * 3)
+    await totalTokensLockedShouldBe(totalAccounts * 1600000 * 3)
 
     await finishCrowdsaleAndIncreaseTimeTo(duration.weeks(1))
 
@@ -289,7 +312,7 @@ contract('OMITokenLock', accounts => {
       await addressBalanceShouldBe(accounts[i], 0)
     }
 
-    await contractBalanceShouldBe(0)
+    await totalTokensLockedShouldBe(0)
 
     console.info(
       `Total gas used for ${totalAccounts} accounts with 3 vesting periods: ${totalGas}`
