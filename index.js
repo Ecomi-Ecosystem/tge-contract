ee6f580eadddd9632bfb46b3a1e150dbda0424c6
JermySu <davis10733@hotmail.com>
2018-06-05 11:31:31 +0800

    Exports contract json files

---
+++ index.js
@@ -0,0 +1,9 @@
+const OMICrowdsale = require('./build/contracts/OMICrowdsale.json')
+const OMIToken = require('./build/contracts/OMIToken.json')
+const OMITokenLock = require('./build/contracts/OMITokenLock.json')
+
+module.exports = {
+  OMICrowdsale,
+  OMIToken,
+  OMITokenLock,
+}
